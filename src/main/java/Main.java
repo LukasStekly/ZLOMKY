ba1eeb3d04a5c5f65bdf39ef5034ee9ff16edd5f
LukasStekly <stekly.luky@seznam.cz>
2026-02-10 13:45:40 +0100

    Implementace zlomku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import cz.uhk.zlomky.data.Zlomek;
+
 public class Main {
 
     public static void main() {
-    IO.println("==========Projekt zlomky==========");  // v java 25 jde takhle napsat nově
+        IO.println("==========Projekt zlomky==========");  // v java 25 jde takhle napsat nově
+        IO.println("__________________________________");
+
+        Zlomek a = new Zlomek(); //konstruktor bez parametru nebo defaultni
+        a.setCitatel(1);
+        a.setJmenovatel(2);
+
+        var b = new Zlomek(3,4);
+
+        IO.println(a);
+        IO.println(b);
+
     }
+
 }
---
+++ src/main/java/cz/uhk/zlomky/data/Zlomek.java
@@ -0,0 +1,36 @@
+package cz.uhk.zlomky.data;
+
+public class Zlomek {
+    private int citatel;
+    private int jmenovatel;
+
+    public Zlomek(){} //defaultni konstruktor
+
+    public Zlomek(int citatel, int jmenovatel)
+    {
+        this.citatel=citatel;
+        this.jmenovatel=jmenovatel;
+    }
+
+    public int getCitatel() {
+        return citatel;
+    }
+
+    public void setCitatel(int citatel) {
+        this.citatel = citatel;
+    }
+
+    public int getJmenovatel() {
+        return jmenovatel;
+    }
+
+    public void setJmenovatel(int jmenovatel) {
+        this.jmenovatel = jmenovatel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%4d\n---------\n%4d", citatel,jmenovatel);
+
+    }
+}
