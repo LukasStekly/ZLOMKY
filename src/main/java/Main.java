26b00f08603def47b1e99ccd3bb8402a1c0c0f6b
LukasStekly <stekly.luky@seznam.cz>
2026-02-17 13:24:16 +0100

    Doplneny pocetni operace

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,47 @@ public class Main {
         a.setCitatel(1);
         a.setJmenovatel(2);
 
-        var b = new Zlomek(3,4);
+        var b = new Zlomek(3, 4);
 
         IO.println(a);
         IO.println(b);
 
+
+        var soucet = a.plus(b);
+        IO.println("Soucet je %s".formatted(a.plus(b).zkratit()));
+
+        IO.println("Rozdil je %s".formatted(a.minus(b).zkratit()));
+
+        IO.println("Soucin je %s".formatted(a.nasobeni(b).zkratit()));
+
+        IO.println("Podil je %s".formatted(a.deleni(b).zkratit()));
+
+        Zlomek[] pole = new Zlomek[]
+                {
+                        new Zlomek (1,3),
+                        new Zlomek (1,2),
+                        new Zlomek (6,5),
+                        new Zlomek (4,3),
+                        new Zlomek (7,8),
+                        new Zlomek (13,10),
+
+                };
+
+        Zlomek vysl = prumer(pole);
+        IO.println("Aritmetický průměr je %s".formatted(vysl));
+
+
+    }
+
+    private static Zlomek prumer(Zlomek[] pole) {
+        var soucet = new Zlomek();
+        for (var z :pole)
+        {
+            soucet = soucet.plus(z);
+        }
+        var pocet = new Zlomek(pole.length, 1);
+
+        return soucet.deleni(pole.length);
     }
 
 }
--- src/main/java/cz/uhk/zlomky/data/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/data/Zlomek.java
@@ -4,7 +4,10 @@ public class Zlomek {
     private int citatel;
     private int jmenovatel;
 
-    public Zlomek(){} //defaultni konstruktor
+    public Zlomek(){
+        citatel = 0;
+        jmenovatel = 1;
+    } //defaultni konstruktor
 
     public Zlomek(int citatel, int jmenovatel)
     {
@@ -28,9 +31,82 @@ public class Zlomek {
         this.jmenovatel = jmenovatel;
     }
 
+
+
+    // operace
+
+    /**
+     * Scita s dalším zlomkem
+     * @param druhy druhy zlomek
+     * @return vysledek jako zlomek
+     */
+    public Zlomek plus (Zlomek druhy)
+    {
+        int cit = citatel * druhy.jmenovatel + druhy.citatel *jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+
+    public Zlomek minus (Zlomek druhy)
+    {
+        int cit = citatel * druhy.jmenovatel - druhy.citatel *jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+
+    public Zlomek nasobeni (Zlomek druhy)
+    {
+        int cit = citatel * druhy.citatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek deleni (Zlomek druhy)
+    {
+        int cit = citatel * druhy.jmenovatel;
+        int jm = jmenovatel * druhy.citatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+
+    public Zlomek zkratit() {
+        var delitel = nsd(citatel,jmenovatel);
+    return new Zlomek (citatel/ delitel, jmenovatel/delitel);
+    }
+
+
+    private int nsd(int a, int b)
+    {
+        if (a>b)
+        {
+            var pom = a;
+            a=b;
+            b=pom;
+
+        }
+
+        int zb;
+        do {
+             zb = a % b;
+             a = b;
+             b = zb;
+        }while (zb != 0);
+        return a;
+        }
+
     @Override
     public String toString() {
-        return String.format("%4d\n---------\n%4d", citatel,jmenovatel);
+        return String.format("%d/%d", citatel,jmenovatel);
 
+    }
+
+    public Zlomek deleni(int num) {
+        return  deleni(new Zlomek(num,1));
     }
 }
